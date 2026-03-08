a8aeb911318c23b5bdc62ea55e77d0a245605d5c
Harnam Singh <harnamsinghsandhu04@gmail.com>
2026-03-08 18:43:34 +0530

    infinite scroll

--- app/page.tsx
+++ app/page.tsx
@@ -3,27 +3,61 @@
 import ProductCardComponent from "@/components/ProductCardComponent";
 import { searchProducts } from "@/lib/api";
 import { ProductCard } from "@/types/products";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { useEffect, useRef } from "react";
 
 export default function Home() {
 
-  const { data, error, isLoading, isError } = useQuery({
-    queryKey: ["search", {}],
-    queryFn: () => searchProducts({}),
-  });
+  const { data, fetchNextPage, hasNextPage, status, isFetchingNextPage } = useInfiniteQuery({
+    queryKey: ["search", ""],
+    queryFn: ({ pageParam = 1 }) => searchProducts({}, pageParam),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, pages) => {
+      if (!lastPage.length) return undefined
+      return pages.length + 1
+    },
+    staleTime: Infinity,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
+    refetchOnMount: false
+  })
+
+  const products = data?.pages.flat()
+
+  const loadMoreRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting && hasNextPage && !isFetchingNextPage) {
+        console.log("Loading more products...");
+        fetchNextPage();
+      }
+    }, {
+      rootMargin: "200px",
+    });
+
+    if (loadMoreRef.current) {
+      observer.observe(loadMoreRef.current);
+    }
+
+    return () => {
+      if (loadMoreRef.current) {
+        observer.unobserve(loadMoreRef.current);
+      }
+    };
+  }, [fetchNextPage, hasNextPage]);
 
   return (
     <>
-      hello world
-      {isLoading && <p>Loading...</p>}
-      {isError && <p>Error: {error.message}</p>}
-      {data && (
+      {products && (
         <ul>
-          {data.map((product: ProductCard) => (
+          {products.map((product: ProductCard) => (
             <ProductCardComponent key={product.id} product={product} />
           ))}
         </ul>
       )}
+      {status === "pending" && <p>Loading...</p>}
+      <div ref={loadMoreRef} className="h-10" />
     </>
   );
 }
